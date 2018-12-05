02df987c8c07b842764e6d3a630f42b5bd4f27c9
mathieu17 <mathieu.ba.17@gmail.com>
2018-12-05 16:39:35 +0100

    ajout de la suppression dans  index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+onst express = require('express');
 const bodyParser = require('body-parser');
 const data = require('./data');
 
@@ -29,6 +29,15 @@ app.post('/', bodyParser.json(), (req, res) => {
     }
   }
   res.status(400).end();
+});
+
+app.delete('/:title', (req, res) => {
+  const index = data.findIndex((d) => d.title === req.params.title);
+  if (index >= 0) {
+    data.splice(index, 1);
+    return res.status(204).end();
+  }
+  res.status(404).end();
 });
 
 app.listen(process.env.PORT, () => console.log('Listening...'));
